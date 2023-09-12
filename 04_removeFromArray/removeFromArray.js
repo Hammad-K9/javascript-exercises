223f311bfcdb56f660b144934f8715001781929e
Hammad-K9 <37565310+Hammad-K9@users.noreply.github.com>
2023-09-12 03:41:40 -0400

    Complete exercises 1-7
    As per the lesson, completed exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  let res = [];
+  arr.forEach((num) => {
+    if (!args.includes(num)) res.push(num);
+  });
+  return res;
 };
 
 // Do not edit below this line
